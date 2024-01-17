da4730628ed8d6dbd41b66d1465fabce90a68e4b
ArvisSilins <144589137+ArvisSilins@users.noreply.github.com>
2024-01-17 21:05:31 +0000

    a not working loop

--- daily.py
+++ daily.py
@@ -18,17 +18,25 @@ time.sleep(2)
 driver.find_element(By.CLASS_NAME, "css-47sehv").click()
 time.sleep(2)
 
-find = driver.find_element(By.XPATH, "//div[@class='media-card']").click()
+loop = driver.find_elements(By.XPATH, "//div[@class='results cover']")
+#find = driver.find_element(By.XPATH, "//div[@class='media-card']").click()
 
-time.sleep(4)
-avarege_score = driver.find_element(By.XPATH,  "//div[@class='el-tooltip data-set']")
-score = avarege_score.find_element(By.XPATH, "//div[@class='el-tooltip data-set']//div[@class='value']")
-print(score.text)
+for ani in loop:
+    time.sleep(4)
 
-genres_ = avarege_score.find_element(By.XPATH, "//div[@class='data-set data-list']//div[@class='type'][contains(text(), 'Genres')]")
-genres = genres_.find_elements(By.XPATH, "following-sibling::div//a")
-for genre in genres:
-    print(genre.text)
+    find = ani.find_element(By.XPATH, "//div[@class='media-card']")
+    find_ = find.find_element(By.XPATH, "following-sibling::div//a").click()
+    time.sleep(3)
 
-#next_anime = find.find_element(By.XPATH, "following-sibling::*[1]")
+    avarege_score = driver.find_element(By.XPATH,  "//div[@class='el-tooltip data-set']")
+    score = avarege_score.find_element(By.XPATH, "//div[@class='el-tooltip data-set']//div[@class='value']")
+    print(score.text)
+
+    genres_ = avarege_score.find_element(By.XPATH, "//div[@class='data-set data-list']//div[@class='type'][contains(text(), 'Genres')]")
+    genres = genres_.find_elements(By.XPATH, "following-sibling::div//a")
+    for genre in genres:
+        print(genre.text)
+
+    driver.back()
+    #next_anime = find.find_element(By.XPATH, "following-sibling::*[1]")
 driver.quit()
