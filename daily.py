c784605b125ac3afc503a89b66d582dabe7b53ce
ArvisSilins <144589137+ArvisSilins@users.noreply.github.com>
2024-01-17 23:24:30 +0000

    still not working loop

--- daily.py
+++ daily.py
@@ -1,11 +1,14 @@
 import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import pandas as pd
 import time
 
-
+options = Options()
+options.add_argument("--ignore-certificate-errors")
 service = Service()
 option = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=option)
@@ -18,14 +21,15 @@ time.sleep(2)
 driver.find_element(By.CLASS_NAME, "css-47sehv").click()
 time.sleep(2)
 
-loop = driver.find_elements(By.XPATH, "//div[@class='results cover']")
-#find = driver.find_element(By.XPATH, "//div[@class='media-card']").click()
+#loop = driver.find_elements(By.XPATH, "//div[@class='results cover']")
+find = driver.find_element(By.XPATH, "//div[@class='media-card']")
+finds = find.find_elements(By.XPATH, ".//a[@class='cover']")
 
-for ani in loop:
+for ani in finds:
     time.sleep(4)
 
-    find = ani.find_element(By.XPATH, "//div[@class='media-card']")
-    find_ = find.find_element(By.XPATH, "following-sibling::div//a").click()
+    ani.click()
+
     time.sleep(3)
 
     avarege_score = driver.find_element(By.XPATH,  "//div[@class='el-tooltip data-set']")
