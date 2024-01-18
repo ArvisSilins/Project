204bfddb00bf2d3175c5a8e305aa0ff2c072cd6f
ArvisSilins <144589137+ArvisSilins@users.noreply.github.com>
2024-01-18 17:03:18 +0000

    loops iet beidzot

--- daily.py
+++ daily.py
@@ -4,12 +4,16 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 import pandas as pd
 import time
 
 options = Options()
 options.add_argument("--ignore-certificate-errors")
-service = Service()
+service = Service(ChromeDriverManager().install())
 option = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=option)
 
@@ -21,26 +25,33 @@ time.sleep(2)
 driver.find_element(By.CLASS_NAME, "css-47sehv").click()
 time.sleep(2)
 
-#loop = driver.find_elements(By.XPATH, "//div[@class='results cover']")
-find = driver.find_element(By.XPATH, "//div[@class='media-card']")
-finds = find.find_elements(By.XPATH, ".//a[@class='cover']")
+while True:
+    try:
+        #loop = driver.find_elements(By.XPATH, "//div[@class='results cover']")
+        find = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='media-card']/a[@class='cover']")))
+        
+        anime_links = [link.get_attribute("href") for link in find]
 
-for ani in finds:
-    time.sleep(4)
+        for anime_link in anime_links:
+            time.sleep(4)
 
-    ani.click()
+            driver.get(anime_link)
 
-    time.sleep(3)
+            time.sleep(3)
 
-    avarege_score = driver.find_element(By.XPATH,  "//div[@class='el-tooltip data-set']")
-    score = avarege_score.find_element(By.XPATH, "//div[@class='el-tooltip data-set']//div[@class='value']")
-    print(score.text)
+            avarege_score = driver.find_element(By.XPATH,  "//div[@class='el-tooltip data-set']")
+            score = avarege_score.find_element(By.XPATH, "//div[@class='el-tooltip data-set']//div[@class='value']")
+            print(score.text)
 
-    genres_ = avarege_score.find_element(By.XPATH, "//div[@class='data-set data-list']//div[@class='type'][contains(text(), 'Genres')]")
-    genres = genres_.find_elements(By.XPATH, "following-sibling::div//a")
-    for genre in genres:
-        print(genre.text)
+            genres_ = avarege_score.find_element(By.XPATH, "//div[@class='data-set data-list']//div[@class='type'][contains(text(), 'Genres')]")
+            genres = genres_.find_elements(By.XPATH, "following-sibling::div//a")
+            for genre in genres:
+                print(genre.text)
 
-    driver.back()
+            driver.back()
+            time.sleep(2) 
+    except NoSuchElementException:
+        print("No more anime to scrape.")
+        break
     #next_anime = find.find_element(By.XPATH, "following-sibling::*[1]")
 driver.quit()
